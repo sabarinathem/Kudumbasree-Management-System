44d5eaa7d031121a14ab1294a9697ed0fa320c87
Sabarinath E M <sabarin992@gmail.com>
2021-10-17 11:35:46 +0530

    display the whole members in the navayuga kudumbasree in index.html by using function index in view.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from app.models import Member
 # Create your views here.
 
 def index(request):
-    return render(request,'app/index.html')
+    members=Member.objects.all()
+    context={'members':members}
+    return render(request,'app/index.html',context)
