95802095785b6262e7cbca647a3682fb85864f43
Sabarinath E M <sabarin992@gmail.com>
2021-10-21 15:32:10 +0530

    completed the modification of index,select month year,add update,savings of intended month

--- app/form.py
+++ app/form.py
@@ -6,6 +6,13 @@ class SavingForm(ModelForm):
     class Meta:
         model=Savings
         fields=('first_weak','second_weak','third_weak','fourth_weak','fifth_weak')
+        widgets={
+            'first_weak':widgets.NumberInput(attrs={'class':'form-control','placeholder':'ഒന്നാം ആഴ്ച'}),
+            'second_weak':widgets.NumberInput(attrs={'class':'form-control','placeholder':'രണ്ടാം ആഴ്ച'}),
+            'third_weak':widgets.NumberInput(attrs={'class':'form-control','placeholder':'മൂന്നാം ആഴ്ച'}),
+            'fourth_weak':widgets.NumberInput(attrs={'class':'form-control','placeholder':'നാലാം ആഴ്ച'}),
+            'fifth_weak':widgets.NumberInput(attrs={'class':'form-control','placeholder':'അഞ്ചാം ആഴ്ച'}),
+        }
 
 class MemberForm(ModelForm):
     class Meta:
--- app/urls.py
+++ app/urls.py
@@ -1,16 +1,17 @@
 from os import name
 from django.urls import path
 from app import views
-
+from django.views.generic import TemplateView
 app_name='app'
 urlpatterns=[
 
     path('',views.startpage,name="startpage"),
+    path('main/',TemplateView.as_view(template_name="app/main.html")),
     path('index/',views.index,name="index"),
     path('addmember/',views.add_member,name="add_member"),
     path('<int:id>/select_month_and_year',views.select_month_and_year,name="select_month_and_year"),
     path('<int:id>/add_or_update_savings',views.add_or_update_savings,name="add_or_update_savings"),
     path('<int:id>/save',views.save,name="save"),
     path('show/',views.show,name="show"),
-    path('savings_of_intended_month/',views.savings_of_intended_month,name="savings_of_intended_month")
+    path('savings_of_intended_month/',views.savings_of_intended_month,name="savings_of_intended_month"),
 ]
